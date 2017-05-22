35711e39514041bc53a8ae37f61d1e688a0491cf
Bastien Libersa <bastien.libersa@gmail.com>
2017-05-22 14:26:01 +0200

    Launches a cluster of NodeJS processes

--- server.js
+++ server.js
@@ -2,36 +2,51 @@ const express     = require('express');
 const wappalyzer  = require('wappalyzer');
 const validUrl    = require('valid-url');
 const bodyParser  = require('body-parser');
+const cluster     = require('cluster');
 
-const PORT = 3001;
+const NUM_CPUS    = require('os').cpus().length;
+const PORT        = 3001;
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.send('OK');
-});
+if(cluster.isMaster) {
+    console.log(`Master cluster setting up ${NUM_CPUS} workers`);
 
-app.post('/extract', function(req, res) {
-  var url = req.body.url;
+    for (let i = 0; i < NUM_CPUS; i++) {
+      cluster.fork();
+    }
 
-  console.log('Extracting technologies for ' + url);
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-  if (validUrl.isUri(url)) {
-    wappalyzer.run([url, '--quiet'], function(stdout, stderr) {
-      if (stdout) {
-        res.send(stdout);
-      }
-      if (stderr) {
-        res.status(400).send(stderr);
+    app.get('/', function(req, res) {
+      res.send('OK');
+    });
+
+    app.post('/extract', function(req, res) {
+      var url = req.body.url;
+
+      console.log(`Extracting technologies for ${url}`);
+
+      if (validUrl.isUri(url)) {
+        wappalyzer.run([url, '--quiet'], function(stdout, stderr) {
+          if (stdout) {
+            res.send(stdout);
+          }
+          if (stderr) {
+            res.sendStatus(400).send(stderr);
+          }
+        });
+      } else {
+        res.sendStatus(422);
       }
     });
-  } else {
-    res.send(422);
-  }
-});
 
-app.listen(PORT);
+    app.listen(PORT);
 
-console.log('Listening on port ' + PORT);
+    console.log(`Worker ${process.pid} listening on port ${PORT}`)
+}
