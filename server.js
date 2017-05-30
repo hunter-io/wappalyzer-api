827996f00c483456eb9121fbaeec4909349283af
Bastien Libersa <bastien.libersa@gmail.com>
2017-05-30 11:28:46 +0200

    Rollbacks to a version without cluster (unstable)

--- server.js
+++ server.js
@@ -4,55 +4,40 @@ const validUrl    = require('valid-url');
 const bodyParser  = require('body-parser');
 const cluster     = require('cluster');
 
-const NUM_CPUS    = require('os').cpus().length;
 const PORT        = 3001;
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-if(cluster.isMaster) {
-    console.log(`Master cluster setting up ${NUM_CPUS} workers`);
+app.get('/', function(req, res) {
+  res.send('OK');
+});
 
-    for (let i = 0; i < NUM_CPUS; i++) {
-      cluster.fork();
-    }
+app.post('/extract', function(req, res) {
+  var url = req.body.url;
+  var timeout = parseInt(req.body.timeout);
 
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`Worker ${worker.process.pid} died`);
-      cluster.fork();
-    });
-} else {
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+  if(isNaN(timeout) || timeout < 5000) {
+    timeout = 5000;
+  }
 
-    app.get('/', function(req, res) {
-      res.send('OK');
-    });
+  console.log(`Extracting technologies for ${url} with a ${timeout}ms timeout`);
 
-    app.post('/extract', function(req, res) {
-      var url = req.body.url;
-      var timeout = parseInt(req.body.timeout);
-
-      if(isNaN(timeout) || timeout < 5000) {
-        timeout = 5000;
+  if (validUrl.isUri(url)) {
+    wappalyzer.run([url, '--quiet', `--resource-timeout=${timeout}`], function(stdout, stderr) {
+      if(stderr) {
+        res.status(400).send(stderr);
       }
-
-      console.log(`Extracting technologies for ${url} with a ${timeout}ms timeout`);
-
-      if (validUrl.isUri(url)) {
-        wappalyzer.run([url, '--quiet', `--resource-timeout=${timeout}`], function(stdout, stderr) {
-          if(stderr) {
-            res.status(400).send(stderr);
-          }
-          else if(stdout) {
-            res.send(stdout);
-          }
-        });
-      } else {
-        res.status(422).end();
+      else if(stdout) {
+        res.send(stdout);
       }
     });
+  } else {
+    res.status(422).end();
+  }
+});
 
-    app.listen(PORT);
+app.listen(PORT);
 
-    console.log(`Worker ${process.pid} listening on port ${PORT}`)
-}
+console.log(`Listening on port ${PORT}`)
