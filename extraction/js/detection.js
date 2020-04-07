641e089e7a29613a85e2c64f038f004e6b2fc574
Antoine Finkelstein <antoine@finkelstein.fr>
2020-04-07 11:34:17 +0200

    Clean repository

--- extraction/js/detection.js
+++
@@ -1,9 +0,0 @@
-function getDetectedApps() {
-  applications = [];
-
-  for (app in wappalyzer.detected[Object.keys(wappalyzer.detected)[0]]) {
-    applications.push(app);
-  }
-
-  return applications;
-}
