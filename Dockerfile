9ad688b76281cd1a30e8c99d718ec3bc8f51054e
Antoine Finkelstein <antoine@finkelstein.fr>
2020-04-07 15:33:23 +0200

    Major refactoring to avoid Selenium

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:12-alpine
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+ENV CHROME_BIN /usr/bin/chromium-browser
+
+RUN apk update && apk add --no-cache \
+	nodejs \
+	nodejs-npm \
+  udev \
+  # chromium \
+  ttf-freefont
+
+ADD *.json /
+ADD *.js /
+
+RUN npm i
+
+RUN /usr/bin/chromium-browser --version
+
+ENTRYPOINT ["node", "app.js"]
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const Wappalyzer = require('wappalyzer')
+
+  // TODO: Handle missing URL
+
+  let url = req.query.url
+
+  if (url == undefined || url == "") {
+    res.status(400).send("missing url query parameter")
+    return
+  }
+
+  const options = {
+    // browser: 'puppeteer',
+    debug: false,
+    maxDepth: 1,
+    recursive: false,
+    maxWait: 20000,
+    userAgent: 'Wappalyzer',
+    htmlMaxCols: 2000,
+    htmlMaxRows: 2000,
+  }
+
+  const wappalyzer = new Wappalyzer(url, options)
+  wappalyzer.analyze()
+    .then((json) => {
+      res.send(`${JSON.stringify(json, null, 2)}`)
+    })
+    .catch((error) => {
+      res.status(500).send(`${error}\n`)
+    })
+})
+
+app.listen(port, () => console.log(`Starting Wappalyzer on http://localhost:${port}`))
